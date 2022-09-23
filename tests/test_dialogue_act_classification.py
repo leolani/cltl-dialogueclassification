0cbe5b9dea60348d64ae09c9db28ef095ef668e7
piekvossen <piekvossen@gmail.com>
2022-09-23 15:44:36 +0200

    - first commit

---
+++ tests/test_dialogue_act_classification.py
@@ -0,0 +1,19 @@
+import unittest
+
+from cltl.dialogue_act_classification.api import DialogueAct
+from cltl.dialogue_act_classification.dialogue_act_classifier import DialogueActDetector
+
+
+class TestEmotions(unittest.TestCase):
+    def setUp(self) -> None:
+        self._dialogue_act_classifier = DialogueActDetector()
+
+    def test_analyze_text_with_emotion(self):
+        acts = self._dialogue_act_classifier.extract_dialogue_act("I am so hapy for you.")
+
+        self.assertEqual(3, len(acts))
+        self.assertEqual("assertion", acts[1].value)
+
+    def test_analyze_empty(self):
+        acts = self._dialogue_act_classifier.extract_dialogue_act("")
+        self.assertEqual(0, len(acts))
