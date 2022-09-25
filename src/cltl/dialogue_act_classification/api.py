695a27b8a37457223d5cb32158bfd3523d9ae6fc
piekvossen <piekvossen@gmail.com>
2022-09-25 13:40:29 +0200

    - first commit

--- src/cltl/dialogue_act_classification/api.py
+++ src/cltl/dialogue_act_classification/api.py
@@ -3,12 +3,30 @@ import dataclasses
 from typing import Optional, Any, List, Union
 from enum import Enum, auto
 
+# (0, 'acknowledge')
+# (1, 'answer')
+# (2, 'backchannel')
+# (3, 'reply_yes')
+# (4, 'exclaim')
+# (5, 'say')
+# (6, 'reply_no')
+# (7, 'hold')
+# (8, 'ask')
+# (9, 'intent')
+# (10, 'ask_yes_no')
 
 class DialogueActType(Enum):
-    GO = auto()
-    EKMAN = auto()
-    FACE = auto()
-    SENTIMENT = auto()
+    acknowledge = auto()
+    answer = auto()
+    backchannel = auto()
+    reply_yes = auto()
+    exclaim = auto()
+    say = auto()
+    reply_no = auto()
+    hold = auto()
+    ask = auto()
+    intent = auto()
+    ask_yes_no = auto()
 
 @dataclasses.dataclass
 class DialogueAct:
