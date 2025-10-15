48b1de7a6ade7f597bc514f675d788ee3a1207b5
piekvossen <piekvossen@gmail.com>
2025-10-15 13:55:05 +0200

    -- the model is now on huggingface!

--- src/cltl/dialogue_act_classification/midas_classifier.py
+++ src/cltl/dialogue_act_classification/midas_classifier.py
@@ -6,6 +6,7 @@ from cltl.dialogue_act_classification.api import DialogueActClassifier, Dialogue
 # based on:
 #https://github.com/DianDYu/MIDAS_dialog_act
 
+## Labels in order from training
 _LABELS={0: 'open_question_factual',
           1: 'pos_answer',
           2: 'command',
@@ -60,10 +61,10 @@ class MidasDialogTagger(DialogueActClassifier):
         print("Loading MIDAS model...", model_path)
         #abs_model_path = os.path.abspath(os.path.expanduser(model_path))
         #self._tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
-        self._tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self._tokenizer = AutoTokenizer.from_pretrained(model_path,  local_files_only=True)
         #self._model = RobertaForSequenceClassification.from_pretrained(abs_model_path, local_files_only=True, use_safetensors=True, num_labels=len(_LABELS))
         #self._pipeline = pipeline("text-classification", model=self._model, tokenizer=self._tokenizer)
-        self._pipeline = pipeline("text-classification", model=model_path)
+        self._pipeline = pipeline("text-classification", model=model_path, top_k=1)
         self._label2id = _LABEL2ID
         self._id2label = _LABELS
         self._dialog =[""] ### initialise with an empty string to get started
@@ -74,15 +75,26 @@ class MidasDialogTagger(DialogueActClassifier):
             return []
         turn0 = self._dialog[-1]
         self._dialog.append(utterance)
+        ## Training:
+        # [('how about another short piece of football news',
+        #   'how can you pick us knows now',
+        #   'open_question_factual'),
+        #  ('do you want to hear some fun facts about cats instead',
+        #   'yes',
+        #   'pos_answer'),
+        #  ('do you want to hear some fun facts about cats instead', 'yes', 'command')]
+        ## strings = [t0 + self._tokenizer.sep_token + t1 for t0, t1, _ in data]
         string = turn0 + self._tokenizer.sep_token + utterance
-        result = self._pipeline(string)
-        dialogueAct = DialogueAct(type="MIDAS", value=result[0]['label'], confidence=float(result[0]['score']))
+        result = self._pipeline(utterance)
+        top_result = result[0]
+        dialogueAct = DialogueAct(type="MIDAS", value=top_result[0]['label'], confidence=float(top_result[0]['score']))
         return [dialogueAct]
 
 if __name__ == "__main__":
     sentences_en = ["I love cats", "Do you love cats?","Yes, I do", "Do you love cats?", "No, dogs"]
     sentences_nl = ["Ik ben dol op katten", "Hou jij van katten?","Ja, ik ben dol op ze", "Hou jij van katten?", "Nee, honden"]
     model_path = "/Users/piek/Desktop/d-Leolani/leolani-models/dialogue_models/midas-da-xlmroberta"
+  #  model_path = "CLTL/midas-da-xlmroberta"
     analyzer = MidasDialogTagger(model_path=model_path)
     for sentence in sentences_en+sentences_nl:
         response = analyzer.extract_dialogue_act(sentence)
